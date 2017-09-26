836e0715700bcd08fcbe224b5baf5e604d55ebf9
JayloneZ <andrezyczkowski@gmail.com>
2017-09-26 16:24:45 +0100

    passed number_to_full

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -30,3 +30,16 @@ end
 def add_string_as_number(num1, num2)
   return num1.to_i+num2.to_i
 end
+
+def number_to_full_month_name(number)
+  case number
+  when 1
+    return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+  else
+    return
+  end
+end
