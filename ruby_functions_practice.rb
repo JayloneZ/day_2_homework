4f66fb0d79019f71c767d7dae0da3eeec7042f14
JayloneZ <andrezyczkowski@gmail.com>
2017-09-26 16:08:18 +0100

    passed 6 tests

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,28 @@
+def return_10()
+return 10
+end
+
+def add(a, b)
+  return a + b
+end
+
+def subtract(a, b)
+  return a - b
+  end
+
+def multiply(a, b)
+  return a * b
+end
+
+def divide(a, b)
+  return a / b
+end
+
+def length_of_string(length)
+  length = "A string of length 21".length
+  return length
+end
+
+def joined_string(string_1, string_2)
+  string = "#{string_1}#{string_2}"
+end
