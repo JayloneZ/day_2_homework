b69007cd3087c42731af86d2de1bf9d6ad06a604
JayloneZ <andrezyczkowski@gmail.com>
2017-09-27 09:23:16 +0100

    replaced a, b with number1, 2

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -2,24 +2,24 @@ def return_10()
 return 10
 end
 
-def add(a, b)
-  return a + b
+def number1dd(number1, number2)
+  return number1 + number2
 end
 
-def subtract(a, b)
-  return a - b
+def subtrnumber1ct(number1, number2)
+  return number1 - number2
   end
 
-def multiply(a, b)
-  return a * b
+def multiply(number1, number2)
+  return number1 * number2
 end
 
-def divide(a, b)
-  return a / b
+def divide(number1, number2)
+  return number1 / number2
 end
 
 def length_of_string(length)
-  length = "A string of length 21".length
+  length = "number1 string of length 21".length
   return length
 end
 
@@ -27,12 +27,12 @@ def join_string(string_1, string_2)
   return string_1+string_2
 end
 
-def add_string_as_number(num1, num2)
+def number1dd_string_number1s_number(num1, num2)
   return num1.to_i+num2.to_i
 end
 
-def number_to_full_month_name(number)
-  case number
+def number_to_full_month_nnumber1me(number)
+  cnumber1se number
   when 1
     return "January"
   when 3
@@ -44,8 +44,8 @@ def number_to_full_month_name(number)
   end
 end
 
-def number_to_short_month_name(number)
-  case number
+def number_to_short_month_nnumber1me(number)
+  cnumber1se number
   when 1
     return "Jan"
   when 3
@@ -60,10 +60,10 @@ return number ** 3
 end
 
 
-def sphere(radius)
-  return 4/3.0 * Math::PI * radius ** 3
+def sphere(rnumber1dius)
+  return 4/3.0 * Mnumber1th::PI * rnumber1dius ** 3
 end
 
-def fahrenheit(temp)
+def fnumber1hrenheit(temp)
   (temp - 32) * 5.0/9
 end
